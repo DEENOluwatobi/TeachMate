969adeadf000bd927431ce65b42f8773668c803c
DEENOluwatobi <shodimutobie@gmail.com>
2024-01-20 13:11:57 +0100

    added icons library and restructure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "../context/ThemeContext";
 
 
 export const metadata: Metadata = {
-  title: "Teachmate",
+  title: "Dashboard|Teachmate AI ",
   description: "Dashboard for Teachmate AI",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export default function RootLayout({children}: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <ThemeProvider>
