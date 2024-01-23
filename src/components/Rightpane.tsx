2e7ea87522b1040dcfd9dac12264ca6c82be8a3b
DEENOluwatobi <shodimutobie@gmail.com>
2024-01-23 03:57:22 +0100

    error fix

--- src/components/Rightpane.tsx
+++ src/components/Rightpane.tsx
@@ -8,15 +8,16 @@ interface RightPaneProps {
 
 const Rightpane: React.FC<RightPaneProps> = ({ children }) => {
   return (
-    <div className='relative flex flex-col gap-1 md:gap-2 h-full'>
+    <div className='relative flex flex-col gap-1 md:gap-2 h-full '>
       <DashNav/>
 
-      <div className='grid md:grid-cols-4 h-full gap-2'>
+      <div className='lg:grid lg:grid-cols-4 h-full gap-2'>
+
         <div className='col-span-4 lg:col-span-3 h-[calc(100vh-5.5em)] overflow-y-scroll rounded-md scroll-smooth'>
           {children}
         </div>
 
-        <div className='lg:col-span-1 h-full rounded-md'>
+        <div className='hidden lg:col-span-1 h-full rounded-md'>
           <SideNav/>
         </div>
       </div>
